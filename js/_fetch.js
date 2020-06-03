05dec8bd93b61c1142cfe3c1adc7a164c2afd764
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-06-03 19:40:46 -0300

    Fix typo pt 2

--- js/_fetch.js
+++ js/_fetch.js
@@ -4,8 +4,8 @@ function _fetch(url, options) {
     const xml = new XMLHttpRequest();
     xml.open(options.mode, url, true);
     if (options.headers) {
-        for (header in options.header)
-            xml.setRequestHeader(header, options.header[header]);
+        for (header in options.headers)
+            xml.setRequestHeader(header, options.headers[header]);
     }
     xml.send(options.body ? options.body : null);
     return new Promise(resolve => xml.onload = resolve.bind(xml, xml.responseText))
