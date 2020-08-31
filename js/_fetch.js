9ecbbe7104ea55b811d364f9347a256b735eabef
João Vaz <joao.guimaraes.alves.vaz@gmail.com>
2020-08-31 09:28:46 -0300

    Minor refactoring

--- js/_fetch.js
+++ js/_fetch.js
@@ -1,12 +1,13 @@
 function _fetch(url, options) {
-    options.mode = !options.mode ? "GET" : options.mode;
+    options.mode = options.mode || 'GET';
 
     const xml = new XMLHttpRequest();
     xml.open(options.mode, url, true);
-    if (options.headers) {
+    
+    if (options.headers)
         for (header in options.headers)
             xml.setRequestHeader(header, options.headers[header]);
-    }
-    xml.send(options.body ? options.body : null);
+    
+    xml.send(options.body || null);
     return new Promise(resolve => xml.onload = resolve.bind(xml, xml.responseText))
 }
